6a3c77b721aaa6dba1cdfbba8dd40921d2d562cd
Calvin J Foster <cal.j.foster@gmail.com>
2019-05-29 12:48:15 -0500

    card button is working

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import 'bootstrap';
 import '../styles/main.scss';
 import movies from './components/movies/movies';
 import location from './components/Locations/locations';
+import view from './components/movies/movieView';
 
 
 const init = () => {
   movies.initializeMovies();
   location.initializeLocations();
+  view.singleMovie();
 };
 
 init();
